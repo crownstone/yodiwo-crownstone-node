4f2cede99121478850b8afbd6f561fd020945b04
Alex de Mulder <alexdemulder@gmail.com>
2018-04-16 12:44:48 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='CrownstoneYodiwo',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
-        'Yodiwo==0.1.0',
+        'Yodiwo==0.1.1',
         'BluenetLib==0.3.0'
     ],
     dependency_links=[
-        'https://github.com/crownstone/yodiwo-python-node/tarball/0.1.0#egg=Yodiwo-0.1.0',
+        'https://github.com/crownstone/yodiwo-python-node/tarball/0.1.1#egg=Yodiwo-0.1.1',
         'https://github.com/crownstone/bluenet-python-lib/tarball/0.3.0#egg=BluenetLib-0.3.0'
     ]
 )

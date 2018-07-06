5563c21c611250363936b3e6cdc35b0e132ec2f9
Alex <AlexDM0@users.noreply.github.com>
2018-07-06 16:28:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     ],
     dependency_links=[
         'https://github.com/crownstone/yodiwo-python-node/tarball/0.1.1#egg=Yodiwo-0.1.1',
-        'https://github.com/crownstone/bluenet-python-lib/tarball/0.3.0#egg=BluenetLib-0.5.0'
+        'https://github.com/crownstone/bluenet-python-lib/tarball/0.5.0#egg=BluenetLib-0.5.0'
     ]
 )

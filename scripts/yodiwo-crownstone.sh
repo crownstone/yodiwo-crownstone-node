715813a4b820bb50d920c7959a365e7e1e71c188
Anne van Rossum <anne@crownstone.rocks>
2018-07-16 15:25:21 +0200

    Operate in subdir of /var/run w.r.t. permissions.

--- scripts/yodiwo-crownstone.sh
+++ scripts/yodiwo-crownstone.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+mkdir /var/run/yodiwo
+chown $USER.$USER /var/run/yodiwo
+
+PID_FILE=/var/run/yodiwo/yodiwo-crownstone.pid
+
 case "$1" in 
 start)
    /usr/bin/python3 /usr/local/bin/yodiwo-crownstone.py --configFile=/etc/yodiwo-crownstone.conf &
-   echo $!>/var/run/yodiwo-crownstone.pid
+   echo $!>$PID_FILE
    ;;
 stop)
-   kill `cat /var/run/yodiwo-crownstone.pid`
-   rm /var/run/yodiwo-crownstone.pid
+   kill `cat $PID_FILE`
+   rm $PID_FILE
    ;;
 restart)
    $0 stop
    $0 start
    ;;
 status)
-   if [ -e /var/run/yodiwo-crownstone.pid ]; then
-      echo yodiwo-crownstone is running, pid=`cat /var/run/yodiwo-crownstone.pid`
+   if [ -e $PID_FILE ]; then
+      echo yodiwo-crownstone is running, pid=`cat $PID_FILE`
    else
       echo yodiwo-crownstone is NOT running
       exit 1

ff7486e36d3454a8f157568c6aa8822ef19ca370
Alex de Mulder <alexdemulder@gmail.com>
2018-07-19 12:32:20 +0200

    bumped dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='CrownstoneYodiwo',
-    version='0.1.2',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
-        'Yodiwo==0.1.1',
-        'BluenetLib==0.5.0'
+        'Yodiwo==0.2.0',
+        'BluenetLib==0.5.1'
     ],
     dependency_links=[
-        'https://github.com/crownstone/yodiwo-python-node/tarball/0.1.1#egg=Yodiwo-0.1.1',
-        'https://github.com/crownstone/bluenet-python-lib/tarball/0.3.0#egg=BluenetLib-0.5.0'
+        'https://github.com/crownstone/yodiwo-python-node/tarball/0.2.0#egg=Yodiwo-0.2.0',
+        'https://github.com/crownstone/bluenet-python-lib/tarball/0.5.1#egg=BluenetLib-0.5.1'
     ]
 )
